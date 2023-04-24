5d713c82adbc78328f9aeb2acaad469245fd8b7d
carlopicazzo <carlopicazzo@gmail.com>
2023-04-25 00:02:18 +0700

    Latest

---
+++ config/database.js
@@ -0,0 +1,8 @@
+import { Sequelize } from "sequelize";
+
+const db = new Sequelize('crud_js', 'root', '$Tr0&G3$T', {
+    host:'clgv1ck0d008kpd9sojj2o7ux',
+    dialect: 'mysql'
+});
+
+export default db;
